9dd18c24846738032482d496c2d073c5187ee832
Ywxig <d.zimnov@gmail.com>
2026-06-03 09:12:34 +0300

    feat: add ability to load files into user directory

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import logging
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import CommandStart
 
+# import os for creating dir
+import os
+
 # import src
 from src import users
 
@@ -35,6 +38,29 @@ async def cmd_start(message: types.Message):
     except Exception as e:
         logger.error(f"Fatal error {e}")
         await message.answer(f"sooting wrong...")
+
+@dp.message(F.document)
+async def handle_file_upload(message: types.Message, bot: Bot):
+    # 1. Get user ID and file name
+    user_id = message.from_user.id
+    file_name = message.document.file_name
+
+    user_dir = f"users/{user_id}/_notes_"
+    
+    file_path = os.path.join(user_dir, file_name)
+    
+    try:
+        # 5. Command the bot to download the file from Telegram servers to your computer/server
+        await bot.download(message.document, destination=file_path)
+        
+        # Log event to info.log
+        logger.info(f"File '{file_name}' saved to directory {user_dir}")
+        await message.answer(f"File successfully saved to your personal folder!")
+        
+    except Exception as e:
+        # If Telegram fails to provide the file or disk space runs out, log to error.log
+        logger.error(f"Failed to save file '{file_name}' for {user_id}. Reason: {e}")
+        await message.answer("Oops! An error occurred while saving the file.")
 
 # Main function for launching
 async def main():
